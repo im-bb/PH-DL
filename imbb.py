5e94d80e3002743094946d94278a31f294dd0639
im-bb <87819247+im-bb@users.noreply.github.com>
2021-08-11 02:24:20 +0530

    Create imbb.py

---
+++ imbb.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import youtube_dl
+def main():
+    print("[PH] Pornhub Simple Downloader | github.com/im-bb") #please dont delete this
+    print("[!] Auto Loop Script, Use Control + C to Stop")
+    directory = input("[*] Folder Directory for Saving : ")
+    while True:
+        url = input("[*] Pornhub Videos Url : ")
+        dire = directory+'/handpicked/%(title)s.%(ext)s'
+        ydl_opts = {
+            'format': 'best',
+            'outtmpl': dire,
+            'nooverwrites': True,
+            'no_warnings': False,
+            'ignoreerrors': True,
+        }
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+
+
+if __name__ == '__main__':
+    main()
